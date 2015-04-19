d2ee988e4639edd34274d545e0dc5cd999508c8a
Jackie Luo <jacquelineluo95@gmail.com>
2015-04-18 23:35:59 -0400

    Add comments

--- coms3134/ps4/SlowMedian.java
+++ coms3134/ps4/SlowMedian.java
@@ -8,6 +8,9 @@ public class SlowMedian implements MovingMedian{
  
     private double[] prices;
  
+    // The method readFile() first reads all of the lines into an ArrayList
+    // as doubles (to get the size), then transfers the doubles to an
+    // array (the static variable prices).
     @Override
     public void readFile(String filename) throws IOException {
         String line = null;
@@ -29,6 +32,12 @@ public class SlowMedian implements MovingMedian{
         }
     }
  
+    // The method getMovingMedians() returns an array of doubles with all of
+    // the moving medians for each window. It iterates over prices, starting
+    // at the end of the first window, and for each window, copies a subarray
+    // of doubles (number) that stores its values. That copy is passed to
+    // the getMedian() method, and the returned moving median is stored in the
+    // medians array.
     @Override
     public double[] getMovingMedians(int window) {
         double[] medians = new double[prices.length - (window - 1)];
@@ -39,6 +48,10 @@ public class SlowMedian implements MovingMedian{
         return medians;
     }
  
+    // The getMedian() method simply returns the median of each window it
+    // receives. With the start and end indexes, it gets the midpoint,
+    // then sorts the subarray for the window and returns the number
+    // at the midpoint index.
     @Override
     public double getMedian(double[] numbers, int start, int end) {
         int mid = (end - start) / 2;
@@ -46,6 +59,8 @@ public class SlowMedian implements MovingMedian{
         return numbers[mid];
     }
     
+    // The printMovingMedians() method was one that I added for convenience,
+    // and it prints all of the values in the medians array, line by line.
     public void printMovingMedians(double[] medians) {
         for(int i = 0; i < medians.length; i++)
             System.out.println(medians[i]);
@@ -53,7 +68,6 @@ public class SlowMedian implements MovingMedian{
     
     public static void main(String[] args) {
         SlowMedian test = new SlowMedian();
-        test.prices = new double[8000];
         try {
             test.readFile(args[0]);
         }
@@ -64,3 +78,5 @@ public class SlowMedian implements MovingMedian{
         test.printMovingMedians(medians);
     }
 }
+
+// The worst-case running time of the program is O(N klogk).
